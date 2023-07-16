172c607ff9dc9692038566f47b70164a16e78293
charu <sonarcharu420@gmail.com>
2023-07-16 22:13:34 +0530

    done

---
+++ hi.py
@@ -0,0 +1,14 @@
+print("Select your Choice:")
+print("1. For Loop")
+print("2. While Loop")
+choice = int( input() )
+if( choice == 1 ):
+    for x in range(1,8,2):
+        print(x)
+elif( choice == 2 ):
+    count=1;
+    while( count< 8):
+        print(count)
+        count+=2;
+else:
+    print( "Ok" )
---
+++ prigra.py
@@ -0,0 +1,9 @@
+import ospath="C:\Users\admin\Documents"
+dir_list=os.listdir(path)
+print("file and directories in''',path,''':")
+print(dir_list)
+
+
+
+
+
